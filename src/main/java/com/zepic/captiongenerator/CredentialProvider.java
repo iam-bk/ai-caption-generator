051a929ae0f436bbaef519edbc5b978f241290fa
BK <bk@zepic.com>
2025-10-23 22:20:14 +0530

    Update credential provider

--- src/main/java/com/zepic/captiongenerator/CredentialProvider.java
+++ src/main/java/com/zepic/captiongenerator/CredentialProvider.java
@@ -2,10 +2,7 @@ package com.zepic.captiongenerator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sagemakerruntime.SageMakerRuntimeClient;
 
@@ -19,7 +16,7 @@ public class CredentialProvider {
 
     @Bean(name="sageMakerClient")
     public SageMakerRuntimeClient sageMakerRuntimeClient() {
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(AwsSessionCredentials.create(ACCESS_KEY_ID, SECRET_ACCESS_KEY, SESSION_TOKEN));
+        AwsCredentialsProvider credentialsProvider = InstanceProfileCredentialsProvider.create();
 
         return SageMakerRuntimeClient.builder()
                 .region(Region.US_WEST_2)
